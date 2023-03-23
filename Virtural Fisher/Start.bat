584bb3bd3fd8f46ca3b8aacaa5e54b23062b3a3d
Jeremiah Banks <79988284+ProfessedRay4@users.noreply.github.com>
2023-03-22 20:51:17 -0500

    main modify

---
+++ Virtural Fisher/Start.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo Downloading libraries...
+pip install pyautogui
+cls
+pip install art
+cls
+pip install colorama
+cls
+pip install progressbar
+cls
+
+
+echo Libraires installed...
+cls
+echo starting main Farm...
+cls
+
+python autofish.py
